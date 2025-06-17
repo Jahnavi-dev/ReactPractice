db2ca1a256dcbc438431489054fb98a2fc3b7614
Jahnavi-dev <jahnavi9322@gmail.com>
2025-06-17 12:18:18 +0530

    react code

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Comp from "./Comp.jsx"
+
+
+function App() {
+
+
+  return (
+    <>
+  <h1>hello</h1>
+  <Comp/>
+    </>
+  )
+}
+
+export default App
---
+++ my-first-app/src/Comp.jsx
@@ -0,0 +1,5 @@
+function Comp (){
+    return <h1>wow</h1>
+}
+
+export default Comp
